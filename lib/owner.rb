bda7ee87247f31b034651120639fea89b7556d84
seerosecode <keshalakemusic@gmail.com>
2019-04-25 19:53:59 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,75 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:cats=>[], :dogs=>[], :fishes=>[]}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |pet_species, list_of_pets|
+      list_of_pets.each do |each_pet|
+        each_pet.mood = "nervous"
+      end
+    end
+    @pets = {}
+  end
+
+  def list_pets
+    dogs = @pets[:dogs].length
+    cats = @pets[:cats].length
+    fish = @pets[:fishes].length
+    return "I have #{fish} fish, #{dogs} dog(s), and #{cats} cat(s)."
+  end
+
 end
